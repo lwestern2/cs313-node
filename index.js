657f365dfccbece23d3885648682d50177e67bd9
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-23 23:40:22 -0600

    prc

--- index.js
+++ index.js
@@ -7,241 +7,187 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get("/getRate", calculateRate )
+  .get("/getRate", calculateRate)
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  function calculateRate(request, response) {
-    var weight = Number(request.query.weight);
-    var mail = request.query.mail;
-    var pieces = Number(request.query.quantity);
-    console.log("weight" + weight + "mail" + mail + "quantity" + pieces);
-    var price;
-    var cost = 0;
-    switch (mail) {
-      case "lettersStamp":
-        if (weight == 1) {
-          cost = .5;
-        } else if (weight == 2) {
-          cost = .71;
-        } else if (weight == 3) {
-          cost = .92;
-        } else if (weight > 3.5) {
-          cost = 1.13;
-        }        
-        console.log("cost = " + cost + " weight  = " + weight);
-        price = pieces * cost;
-        break;
-      case "lettersMetered":
-        if (weight == 1) {
-          cost = .47;
-        } else if (weight == 2) {
-          cost = .68;
-        } else if (weight == 3) {
-          cost = .89;
-        } else if (weight > 3.5) {
-          cost = 1.10;
-        }
-        console.log("cost  = " + cost + " weight  = " + weight);
-         price = pieces * cost;
-        console.log("price = " + price);
-        break;
-      case "largeEnvelopsFlats":
-        switch (weight) {
-          case 1:
-            cost = 1;
-            console.log("cost  = " + cost + " weight  = " + weight);
-            price = pieces * cost;
-            console.log("price = " + price);
-            break;
-           
-          case 2:
-            cost = 1.21;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 3:
-            cost = 1.42
-            console.log("cost  = " + cost + " weight  = " + weight);
-            price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 4:
-            cost = 1.63;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 5:
-            cost = 1.84;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 6:
-            cost = 2.05;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 7:
-            cost = 2.26;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 8:
-            cost = 2.47;
-            console.log("cost  = " + cost + " weight  = " + weight);
-           price = pieces * cost;
-            console.log("price = " + price);
-            break;
-           
-          case 9:
-            cost = 2.68;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 10:
-            cost = 2.89;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 11:
-            cost = 3.10;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 12:
-            cost = 3.31;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          default:
-            cost = 3.52;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;            
-        }
+function calculateRate(request, response) {
+  var weight = Number(request.query.weight);
+  var mail = request.query.mail;
+  var pieces = Number(request.query.quantity);
+  var price;
+  var cost = 0;
+  switch (mail) {
+    case "Letters Stamped":
+      if (weight == 1) {
+        cost = .55;
+      } else if (weight == 2) {
+        cost = .70;
+      } else if (weight == 3) {
+        cost = .85;
+      } else if (weight > 3.5) {
+        cost = 1.00;
+      }
+      price = pieces * cost;
+      break;
+    case "Letters Metered":
+      if (weight == 1) {
+        cost = .50;
+      } else if (weight == 2) {
+        cost = .65;
+      } else if (weight == 3) {
+        cost = .80;
+      } else if (weight > 3.5) {
+        cost = .95;
+      }
+      price = pieces * cost;
+      break;
+    case "Large Envelopes Flats":
+      switch (weight) {
+        case 1:
+          cost = 1;
+          price = pieces * cost;
+          break;
 
-      case "firstClassPackageService":
-        switch (weight) {
-          case 1:
-            cost = 3.5;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 2:
-            cost = 3.50;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-          
-          case 3:
-            cost = 3.5;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 4:
-            cost = 3.5;
-            console.log("cost  = " + cost + " weight  = " + weight);
-           price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 5:
-            cost = 3.75;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 6:
-            cost = 3.75;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 7:
-            cost = 3.75;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 8:
-            cost = 3.75;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          case 9:
-            cost = 4.10;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-          
-          case 10:
-            cost = 4.45;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;   
+        case 2:
+          cost = 1.15;
+          price = pieces * cost;
+          break;
 
-          case 11:
-            cost = 4.80;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-           
-          case 12:
-            cost = 5.15;
-            console.log("cost  = " + cost + " weight  = " + weight);
-             price = pieces * cost;
-            console.log("price = " + price);
-            break;
-            
-          default:
-            cost = 5.50;
-            console.log("cost  = " + cost + " weight  = " + weight);
-            price = pieces * cost;
-            console.log("price = " + price);
-            break;            
-        }
-    }
+        case 3:
+          cost = 1.30;
+          price = pieces * cost;
+          break;
 
-    console.log(price);
-    var params = {
-        weight: weight,
-        mail: mail,
-        price: price,
-        quantity: pieces
-    };
-    response.render("result", params);
-        
+        case 4:
+          cost = 1.45;
+          price = pieces * cost;
+          break;
+
+        case 5:
+          cost = 1.60;
+          price = pieces * cost;
+          break;
+
+        case 6:
+          cost = 1.75;
+          price = pieces * cost;
+          break;
+
+        case 7:
+          cost = 1.90;
+          price = pieces * cost;
+          break;
+
+        case 8:
+          cost = 2.05;
+          price = pieces * cost;
+          break;
+
+        case 9:
+          cost = 2.20;
+          price = pieces * cost;
+          break;
+
+        case 10:
+          cost = 2.35;
+          price = pieces * cost;
+          break;
+
+        case 11:
+          cost = 2.50;
+          price = pieces * cost;
+          break;
+
+        case 12:
+          cost = 2.65;
+          price = pieces * cost;
+          break;
+
+        default:
+          cost = 2.80;
+          price = pieces * cost;
+          break;
+      }
+      break;
+
+    case "First Class Package Service":
+      switch (weight) {
+        case 1:
+          cost = 3.66;
+          price = pieces * cost;
+          break;
+
+        case 2:
+          cost = 3.66;
+          price = pieces * cost;
+          break;
+
+        case 3:
+          cost = 3.66;
+          price = pieces * cost;
+          break;
+
+        case 4:
+          cost = 3.66;
+          price = pieces * cost;
+          break;
+
+        case 5:
+          cost = 4.39;
+          price = pieces * cost;
+          break;
+
+        case 6:
+          cost = 4.39;
+          price = pieces * cost;
+          break;
+
+        case 7:
+          cost = 4.39;
+          price = pieces * cost;
+          break;
+
+        case 8:
+          cost = 4.39;
+          price = pieces * cost;
+          break;
+
+        case 9:
+          cost = 5.19;
+          price = pieces * cost;
+          break;
+
+        case 10:
+          cost = 5.19;
+          price = pieces * cost;
+          break;
+
+        case 11:
+          cost = 5.19;
+          price = pieces * cost;
+          break;
+
+        case 12:
+          cost = 5.19;
+          price = pieces * cost;
+          break;
+
+        default:
+          cost = 5.71;
+          price = pieces * cost;
+          break;
+      }
+      break;
+  }
+
+  console.log(price);
+  var params = {
+    weight: weight,
+    mail: mail,
+    price: price,
+    quantity: pieces
   };
+  response.render("pages/result", params);
+
+};
